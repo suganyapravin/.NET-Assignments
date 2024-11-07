cb6a11f741b4b7d7f3a1f223a5c431a218176db8
Suganya <59360647+suganyapravin@users.noreply.github.com>
2024-11-07 11:54:31 -0500

    MVC web application

---
+++ Requirement 5/MVCApplication/Data/MVCApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCApplication.Models;
+
+namespace MVCApplication.Data
+{
+    public class MVCApplicationContext : DbContext
+    {
+        public MVCApplicationContext (DbContextOptions<MVCApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCApplication.Models.User> User { get; set; } = default!;
+    }
+}
